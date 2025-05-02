79f2d7e7c55d222795cd38d94342b04754cf0d0c
Krishan <krishandwive9@gmail.com>
2025-05-03 00:57:12 +0530

    get request working

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    console.log("Error in getting products", error.message);
+    res.status(500).json({ success: false, message: "server Error" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = req.body;
 
@@ -24,6 +34,17 @@ app.post("/api/products", async (req, res) => {
   } catch (error) {
     console.error("Error in Creating product", error.message);
     res.status(500).json({ success: false, message: "server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
   }
 });
 
